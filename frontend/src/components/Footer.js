099d53cbbdcd803f1ad4e1d6caefa2bc5430737b
Saikumar <saikumarkaleru@Saikumars-MacBook-Air.local>
2022-02-17 22:36:24 +0530

    msg

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<Col className='text-center py-3'>
+						
+						<a
+							rel='noopener noreferrer'
+							href='https://github.com/'
+							target='_blank'
+							className='melvin-kisten'
+						>
+							<i className='fas fa-user-circle'></i>  Github
+						</a>
+						Copyright &copy; React E-Commerce
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	)
+}
+
+export default Footer
