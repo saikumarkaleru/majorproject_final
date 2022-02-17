802ffc350458a4226dab16145bb3628ed7f6aa0c
Saikumar <saikumarkaleru@Saikumars-MacBook-Air.local>
2022-02-17 22:59:28 +0530

    msg

---
+++ docker_images.sh
@@ -0,0 +1,13 @@
+
+
+docker login -u 9398320218 -p Sai@12345
+
+cd backend
+docker build -t 9398320218/mp_backend .
+docker push 9398320218/mp_backend
+
+cd ..
+cd frontend
+docker build -t 9398320218/mp_frontend.
+docker push 9398320218/mp_frontend
+
