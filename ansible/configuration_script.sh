b034869af9f64d89d4d8563a60d15a09027b1f40
sai <saik@gmail.com>
2022-03-05 13:29:01 +0530

    ingress

--- ansible/configuration_script.sh
+++ ansible/configuration_script.sh
@@ -10,7 +10,7 @@ echo 'Docker instlalation'
 sudo apt-get update
 
 #sudo yum -y install docker
-#sudo apt install docker.io -y
+sudo apt install docker.io -y
 #sudo service docker start
 #sudo service enable docker
 sudo systemctl start docker
@@ -25,25 +25,65 @@ sudo usermod -a -G docker ubuntu
 #sudo usermod -aG docker $(whoami)
 sudo chmod 666 /var/run/docker.sock
 
+sudo su 
+sudo cat<<EOF > /etc/docker/daemon.json
+{
+   "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+sudo systemctl restart docker
+
 
 echo 'KUBERENETES INSTALLATION'
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
 
 #sudo kubeadm reset
 
 sudo kubeadm init
+
+exit 
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+#sleep 20
+
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
 
 sleep 20
 
 #node name -- private ip
 #
-hostname=$(hostname)
-kubectl taint nodes  $hostname node-role.kubernetes.io/master-
+
+#hostname=$(hostname)
+#kubectl taint nodes  $hostname node-role.kubernetes.io/master-
+
 
 sudo apt install openjdk-11-jre -y 
 
